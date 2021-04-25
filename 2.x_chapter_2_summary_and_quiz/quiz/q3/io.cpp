c2308794209761faef774e17ed105701568dadbe
jatrindo <jatrindo4096@gmail.com>
2021-04-25 14:04:50 -0500

    Include first draft answer for Q3 of 2.x Quiz

---
+++ 2.x_chapter_2_summary_and_quiz/quiz/q3/io.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int readNumber()
+{
+	std::cout << "Enter a number: ";
+	int n {};
+	std::cin >> n;
+	return n;
+}
+
+void writeAnswer(int ans)
+{
+	std::cout << "The answer is " << ans << '\n';
+	return;
+}
+
