eb75c190edc3a63c8ab57fe98ef7dacc8fd7abf2
jatrindo <jatrindo4096@gmail.com>
2021-04-27 15:51:09 -0500

    Include initial problem code and Makefile for Q1 of 3.x Quiz

---
+++ 3.x_chapter_3_summary_and_quiz/quiz/q1/program.cpp
@@ -0,0 +1,29 @@
+/*
+ * Use the (integrated) debugger to step through this program and watch the
+ * value of x.
+ * Based on the information you learn, fix the following program.
+*/
+
+#include <iostream>
+
+int readNumber(int x)
+{
+	std::cout << "Please enter a number: ";
+	std::cin >> x;
+	return x;
+}
+
+void writeAnswer(int x)
+{
+	std::cout << "The sum is:" << x;
+}
+
+int main()
+{
+	int x {};
+	readNumber(x);
+	x = x + readNumber(x);
+	writeAnswer(x);
+
+	return 0;
+}
