d895d99cfcab781a9140040d17e214c95cce9c00
jatrindo <jatrindo4096@gmail.com>
2021-05-01 17:59:37 -0500

    Include first draft solution for Q2 of 5.3 Quiz

--- 5.3_modulus_and_exponentiation/quiz/q2/program.cpp
+++ 5.3_modulus_and_exponentiation/quiz/q2/program.cpp
@@ -6,3 +6,28 @@
  * it is even, and false otherwise. Use the modulus operator to test whether
  * the integer parameter is even.
 */
+#include <iostream>
+
+bool isEven(int x)
+{
+	return (x % 2) == 0;
+}
+
+int main()
+{
+	std::cout << "Enter an integer: ";
+
+	int i{};
+	std::cin >> i;
+
+	if (isEven(i))
+	{
+		std::cout << i << " is even\n";
+	}
+	else
+	{
+		std::cout << i << " is odd\n";
+	}
+
+	return 0;
+}
