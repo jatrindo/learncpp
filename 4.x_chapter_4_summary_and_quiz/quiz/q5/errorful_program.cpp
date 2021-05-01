f8030b2a1ffeff5cc63ce7a5ac26e70b31ed7194
jatrindo <jatrindo4096@gmail.com>
2021-04-30 20:46:12 -0500

    Corrected first draft solution for Q5 of 4.x Quiz using the provided solution code

--- 4.x_chapter_4_summary_and_quiz/quiz/q5/errorful_program.cpp
+++ 4.x_chapter_4_summary_and_quiz/quiz/q5/errorful_program.cpp
@@ -8,17 +8,37 @@ int main()
 {
 	std::cout << "How old are you?\n";
 
-	std::uint8_t age{}; // Error 1: No need (+ risky) to use an _unsigned_ int (and an 8b one at that)
-	std::cin >> age; // Error 2: Use of *int8_t is a problem since it will be interpreted as a character
+	std::uint8_t age{}; // Error 1
+	std::cin >> age;
 
 	std::cout << "Allowed to drive a car in Texas [";
 
-	if (age >= 16) // Error 3: The interpretation of age as a character means that the decimal value of the corresponding ASCII character will be compared against 16
-		std::cout << "x";
+	if (age >= 16) // Error 3
+		std::cout << "x"; // Error 2, pt 1
 	else
-		std::cout << " ";
+		std::cout << " "; // Error 2, pt 2
 
 	std::cout << "]\n";
 
 	return 0;
 }
+
+/*
+ * Explanations:
+ *
+ * Error 1:
+ * 	- std::uint8_t is an optional type; not all systems support it
+ * 	- If we were to print 'age' it would be treated as a character by std::cout
+ * 	- A regular 'int' should be used to store the age. Age doesn't require a specific
+ * 		minimum integer width
+ *
+ * Error 2, pt 1 & 2:
+ * 	- The characters 'x' and ' ' should be in single quotes, rather than double quotes
+ * 		- Double-quoting 'x' and ' ' requires more storage and cause slower
+ * 		functions to be called than if we used single quotation marks
+ *
+ * Error 3:
+ * 	- '16' can be seen as a magic number
+ * 		- As per best practice, a constexpr variable with the value 16 should be
+ * 		defined and used instead
+*/
