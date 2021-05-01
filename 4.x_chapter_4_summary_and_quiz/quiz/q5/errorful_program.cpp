0719f895cb00a2b4ef97f64ae69351f4e59dfe0d
jatrindo <jatrindo4096@gmail.com>
2021-04-30 20:37:47 -0500

    Include first draft solution for Q5 of 4.x Quiz

--- 4.x_chapter_4_summary_and_quiz/quiz/q5/errorful_program.cpp
+++ 4.x_chapter_4_summary_and_quiz/quiz/q5/errorful_program.cpp
@@ -8,12 +8,12 @@ int main()
 {
 	std::cout << "How old are you?\n";
 
-	std::uint8_t age{};
-	std::cin >> age;
+	std::uint8_t age{}; // Error 1: No need (+ risky) to use an _unsigned_ int (and an 8b one at that)
+	std::cin >> age; // Error 2: Use of *int8_t is a problem since it will be interpreted as a character
 
 	std::cout << "Allowed to drive a car in Texas [";
 
-	if (age >= 16) 
+	if (age >= 16) // Error 3: The interpretation of age as a character means that the decimal value of the corresponding ASCII character will be compared against 16
 		std::cout << "x";
 	else
 		std::cout << " ";
