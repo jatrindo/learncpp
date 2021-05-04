261f204f342b7ab67aa391ea9727e78e32569b0a
jatrindo <jatrindo4096@gmail.com>
2021-05-04 13:37:53 -0500

    Include initial problem code and Makefile for Q6 of O.4 Quiz

---
+++ O.4_converting_between_binary_and_decimal/quiz/q6/print_as_binary.cpp
@@ -0,0 +1,5 @@
+/*
+ * Write a program that asks the user to input a number between 0 and 255.
+ * Print this number as an 8-bit binary number (of the form #### ####).
+ * Don't use any bitwise operators. Don't use std::bitset
+*/
