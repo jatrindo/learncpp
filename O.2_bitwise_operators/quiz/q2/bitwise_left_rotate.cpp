45e6b216845538105332e49a34ffdf509f7bce68
jatrindo <jatrindo4096@gmail.com>
2021-05-03 22:35:32 -0500

    Include first draft solution for Q2 of O.2 Quiz

---
+++ O.2_bitwise_operators/quiz/q2/bitwise_left_rotate.cpp
@@ -0,0 +1,27 @@
+#include <bitset>
+#include <iostream>
+
+std::bitset<4> rotl(std::bitset<4> bits)
+{
+	// Your code here -- okay to use bitset.test() and set()
+	bool leftmost_bit { bits.test(3) };
+
+	bits = bits << 1;
+
+	// If the leftmost bit was set before, it'll be the rightmost bit now
+	if (leftmost_bit)
+		bits.set(0);
+
+	return bits;
+}
+
+int main()
+{
+	std::bitset<4> bits1{ 0b0001 };
+	std::cout << rotl(bits1) << '\n';
+
+	std::bitset<4> bits2{ 0b1001 };
+	std::cout << rotl(bits2) << '\n';
+
+	return 0;
+}
