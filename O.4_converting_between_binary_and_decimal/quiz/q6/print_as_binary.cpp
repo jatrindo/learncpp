e69a9a351eb853e9ea3697dd5f8c0ecf9faf4981
jatrindo <jatrindo4096@gmail.com>
2021-05-04 14:50:58 -0500

    Include second draft solution for Q6 of O.4 after referencing solution code

--- O.4_converting_between_binary_and_decimal/quiz/q6/print_as_binary.cpp
+++ O.4_converting_between_binary_and_decimal/quiz/q6/print_as_binary.cpp
@@ -13,38 +13,43 @@ int ask_number()
 	return n;
 }
 
-// Prints '1' or '0' on whether i fits the given power of 2
-// ('1' if it does, '0' if it doesn't)
+// Prints either a '1' or '0' and returns either a decremented i, depending on
+// whether the given power of 2 'fits' within (i.e. is less than) i
 //
-// If i fits, it returns i decremented by the power of 2, otw 
-// returning just i. unaltered
-int print_and_return_decremented_if_fit(int i, int pwr_of_2) 
+// Prints '1' if the given power of 2 'fits' within i
+// Prints '0' if the given power of 2 is larger than i (i.e. does not fit)
+//
+// If the given power of 2 fits within i, we return i decremented by the
+// given power of two.
+// Otherwise, we return i, unaltered
+int print_and_decrement_if_fits(int i, int pwr_of_2)
 {
-	if (i < pwr_of_2)
+	if (i >= pwr_of_2)
 	{
-		std::cout << '0';
-		return i;
+		// Power of 2 does fit within i
+		std::cout << '1';
+		return i - pwr_of_2;
 	}
 
-	std::cout << '1';
-	return i - pwr_of_2;
+	// Power of 2 does not fit within i
+	std::cout << '0';
+	return i;
 }
 
 // Assumes i is within the range of [0,255]
 void print_as_binary(int i)
 {
-	i = print_and_return_decremented_if_fit(i, 128);
-	i = print_and_return_decremented_if_fit(i, 64);
-	i = print_and_return_decremented_if_fit(i, 32);
-	i = print_and_return_decremented_if_fit(i, 16);
+	i = print_and_decrement_if_fits(i, 128);
+	i = print_and_decrement_if_fits(i, 64);
+	i = print_and_decrement_if_fits(i, 32);
+	i = print_and_decrement_if_fits(i, 16);
 	std::cout << ' ';
-	i = print_and_return_decremented_if_fit(i, 8);
-	i = print_and_return_decremented_if_fit(i, 4);
-	i = print_and_return_decremented_if_fit(i, 2);
-	i = print_and_return_decremented_if_fit(i, 1);
+	i = print_and_decrement_if_fits(i, 8);
+	i = print_and_decrement_if_fits(i, 4);
+	i = print_and_decrement_if_fits(i, 2);
+	i = print_and_decrement_if_fits(i, 1);
 	std::cout << '\n';
 }
-
 
 int main()
 {
