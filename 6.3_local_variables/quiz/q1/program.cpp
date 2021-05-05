4b548ff83484a8c7b9b0b63ba89ff9d90ed4537b
jatrindo <jatrindo4096@gmail.com>
2021-05-04 22:21:54 -0500

    Include first draft solution for Q1 of 6.3 Quiz

--- 6.3_local_variables/quiz/q1/program.cpp
+++ 6.3_local_variables/quiz/q1/program.cpp
@@ -10,3 +10,34 @@
  *
  * Add comments to your code indicating where each variable dies
  */
+#include <iostream>
+
+int getInteger()
+{
+	int i{};
+	std::cin >> i;
+	return i;
+} // 'i' dies here
+
+
+int main()
+{
+	std::cout << "Enter an integer: ";
+	int smaller{ getInteger() };
+
+	std::cout << "Enter a larger integer: ";
+	int larger{ getInteger() };
+
+	if (smaller > larger)
+	{
+		std::cout << "Swapping the values...\n";
+		int temp{ smaller };
+		smaller = larger;
+		larger = temp;
+	} // 'temp' dies here
+
+	std::cout << "The smaller value is " << smaller << '\n';
+	std::cout << "The larger value is " << larger << '\n';
+
+	return 0;
+} // 'smaller' and 'larger' die here
