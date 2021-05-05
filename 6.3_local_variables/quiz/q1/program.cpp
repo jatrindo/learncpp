25aa4003bccd9d550d982fa63dee17d10a0295b7
jatrindo <jatrindo4096@gmail.com>
2021-05-04 22:14:54 -0500

    Include initial problem statement and Makefile for Q1 of 6.3 Quiz

---
+++ 6.3_local_variables/quiz/q1/program.cpp
@@ -0,0 +1,12 @@
+/*
+ * Write a program that asks the user to enter two integers, one named
+ * 'smaller', the other named 'larger'
+ *
+ * If the user enters a smaller value for the second integer, use a block
+ * and temporary variable to swap the smaller and larger values (so that
+ * smaller value is in 'smaller' and the larger value in 'larger')
+ *
+ * Then print the values of the 'smaller' and 'larger' variables.
+ *
+ * Add comments to your code indicating where each variable dies
+ */
