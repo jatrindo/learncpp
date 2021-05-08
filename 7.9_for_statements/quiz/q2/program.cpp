0605f89633eda6f977d48b874d48182f76c86a9e
jatrindo <jatrindo4096@gmail.com>
2021-05-08 18:51:50 -0500

    Include first draft solution for Q2 of 7.9 Quiz

--- 7.9_for_statements/quiz/q2/program.cpp
+++ 7.9_for_statements/quiz/q2/program.cpp
@@ -4,3 +4,28 @@
  *
  * For example, sumTo(5) should return 15, which is 1 + 2 + 3 + 4 + 5
 */
+#include <iostream>
+
+int sumTo(int value)
+{
+	int sum{ 0 };
+	for (int i{ 1 }; i <= value; i++)
+		sum += i;
+	return sum;
+}
+
+int askInteger()
+{
+	std::cout << "Enter an integer: ";
+	int i{};
+	std::cin >> i;
+	return i;
+}
+
+int main()
+{
+	int value{ askInteger() };
+	std::cout << sumTo(value) << '\n';
+
+	return 0;
+}
