1888332b3a77dce44d5bb114f96889bd222cb523
jatrindo <jatrindo4096@gmail.com>
2021-05-08 13:51:19 -0500

    Include first draft solution for Q2 of 7.7 Quiz

--- 7.7_intro_to_loops_and_while_statements/quiz/q2/program.cpp
+++ 7.7_intro_to_loops_and_while_statements/quiz/q2/program.cpp
@@ -2,3 +2,17 @@
  * Write a program that prints out the letters 'a' through 'z' along with
  * their ASCII codes
 */
+#include <iostream>
+
+int main()
+{
+	char c{ 'a' };
+
+	while ( c <= 'z' )
+	{
+		std::cout << "Char: " << c << ", ASCII Code: " << static_cast<int>(c) << '\n';
+		c += 1;
+	}
+
+	return 0;
+}
