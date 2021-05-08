38931f8fcc2215cb6381c8e496aa9152b343a875
jatrindo <jatrindo4096@gmail.com>
2021-05-08 18:46:56 -0500

    Include initial problem statement and Makefile for Q2 of 7.9 Quiz

---
+++ 7.9_for_statements/quiz/q2/program.cpp
@@ -0,0 +1,6 @@
+/*
+ * Write a function named sumTo() that takes an integer parameter named
+ * 'value', and returns the sum of all numbers from 1 to value
+ *
+ * For example, sumTo(5) should return 15, which is 1 + 2 + 3 + 4 + 5
+*/
