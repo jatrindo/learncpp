3c4a68f0809a4a4756610310e1013f5d8dba2d34
jatrindo <jatrindo4096@gmail.com>
2021-05-08 14:31:52 -0500

    Include first draft solution for Q4 of 7.7 Quiz

--- 7.7_intro_to_loops_and_while_statements/quiz/q4/program.cpp
+++ 7.7_intro_to_loops_and_while_statements/quiz/q4/program.cpp
@@ -7,3 +7,33 @@
  *   4 3 2 1
  * 5 4 3 2 1
 */
+#include <iostream>
+
+int main()
+{
+	constexpr int num_columns{ 5 };
+
+	int outer{ 1 };
+	while ( outer <= num_columns )
+	{
+		int inner{ num_columns };
+		while ( inner > 0)
+		{
+			if (inner > outer)
+			{
+				std::cout << ' ' << ' ';
+			}
+			else
+			{
+				std::cout << inner << ' ';
+			}
+
+			inner--;
+		}
+
+		std::cout << '\n';
+		outer++;
+	}
+
+	return 0;
+}
