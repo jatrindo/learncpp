46e2142fe406e25ba84d35978b8c5b1ce861d7e9
jatrindo <jatrindo4096@gmail.com>
2021-05-08 11:25:40 -0500

    Include initial problem statement and Makefile for Q1 of 7.5 Quiz

---
+++ 7.5_switch_fallthrough_and_scoping/q1/program.cpp
@@ -0,0 +1,8 @@
+/*
+ * Write a function called calculate() that takes two integers and a char
+ * representing one of teh following mathematical operations: +, -, *, /, or
+ * % (modulus). Use a switch statement to perform the appropriate mathematical
+ * operation on the integers, and return the result. If an invalid operator
+ * is passed into the function, the function should print an error. For the
+ * division operator, do an integer division.
+*/
