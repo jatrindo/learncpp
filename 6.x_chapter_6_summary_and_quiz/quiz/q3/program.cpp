2a783f052c0fc755c69ba9572607da7b28ac2736
jatrindo <jatrindo4096@gmail.com>
2021-05-08 00:14:18 -0500

    Include initial problem code, problem statement, and Makefile for Q3 of 6.x Quiz

---
+++ 6.x_chapter_6_summary_and_quiz/quiz/q3/program.cpp
@@ -0,0 +1,16 @@
+/*
+ * Complere the following program by writing the passOrFail() function,
+ * which should return true for the first 3 calls, and false thereafter.
+*/
+#include <iostream>
+
+int main()
+{
+	std::cout << "User #1: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	std::cout << "User #2: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	std::cout << "User #3: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	std::cout << "User #4: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	std::cout << "User #5: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	
+	return 0;
+}
