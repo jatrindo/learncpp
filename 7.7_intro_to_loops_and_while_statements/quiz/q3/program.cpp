30543c7553f8265e7d883fee4f762734312eb3d2
jatrindo <jatrindo4096@gmail.com>
2021-05-08 14:00:16 -0500

    Include first draft solution for Q3 of 7.7 Quiz

--- 7.7_intro_to_loops_and_while_statements/quiz/q3/program.cpp
+++ 7.7_intro_to_loops_and_while_statements/quiz/q3/program.cpp
@@ -7,3 +7,25 @@
  * 2 1
  * 1
 */
+#include <iostream>
+
+int main()
+{
+	constexpr int num_lines{ 5 };
+
+	int i{ num_lines };
+	while (i > 0)
+	{
+		int j{ i };
+		while (j > 0)
+		{
+			std::cout << j << ' ';
+			j -= 1;
+		}
+		std::cout << '\n';
+
+		i -= 1;
+	}
+
+	return 0;
+}
