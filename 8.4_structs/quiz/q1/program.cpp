6d09d26f84e74918ecd15c20926aad861b4b9530
jatrindo <jatrindo4096@gmail.com>
2021-05-11 20:24:26 -0500

    Include first draft solution for Q1 of 8.4 Quiz

--- 8.4_structs/quiz/q1/program.cpp
+++ 8.4_structs/quiz/q1/program.cpp
@@ -10,3 +10,52 @@
  * struct to a function that prints each of the values and then calculates how
  * much you made that day (multiply all 3 fields together)
 */
+#include <iostream>
+
+struct AdStats
+{
+	int ads_shown{};
+	double percent_ads_clicked{};
+	double avg_earned_per_click{};
+};
+
+AdStats askAdStats()
+{
+	std::cout << "# ads shown?: ";
+	int ads_shown;
+	std::cin >> ads_shown;
+
+	std::cout << "% of ads clicked?: ";
+	double percent_ads_clicked{};
+	std::cin >> percent_ads_clicked;
+
+	std::cout << "Avg earned per click (in dollars)?: ";
+	double avg_earned_per_click{};
+	std::cin >> avg_earned_per_click;
+
+	return AdStats{ads_shown, percent_ads_clicked / 100.0, avg_earned_per_click};
+}
+
+float calculateDayRevenue(AdStats adstats)
+{
+	return adstats.ads_shown
+		* adstats.percent_ads_clicked
+		* adstats.avg_earned_per_click;
+}
+void printAdStatsAndDayRevenue(AdStats adstats)
+{
+	std::cout << "\n=========== TODAY'S REVENUE: ===========\n";
+	std::cout << "# ads shown: " << adstats.ads_shown << '\n';
+	std::cout << "% of ads clicked: " << adstats.percent_ads_clicked << '\n';
+	std::cout << "Avg earned per click: " << adstats.avg_earned_per_click << '\n';
+	std::cout << "\nTotal revenue for day: " << calculateDayRevenue(adstats) << '\n';
+	std::cout << "========================================\n";
+}
+
+int main()
+{
+	AdStats adstats{ askAdStats() };
+	printAdStatsAndDayRevenue(adstats);
+
+	return 0;
+}
