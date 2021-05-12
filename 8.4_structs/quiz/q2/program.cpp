1e3cf104cc95cedf27297e23921033e0924c6f37
jatrindo <jatrindo4096@gmail.com>
2021-05-11 20:36:10 -0500

    Include initial problem statement and Makefile for Q2 of 8.4 Quiz

---
+++ 8.4_structs/quiz/q2/program.cpp
@@ -0,0 +1,12 @@
+/*
+ * Create a struct to hold a fraction.
+ *
+ * The struct should have an integer numerator and an integer denominator
+ * member. Declare 2 fraction variables and read them in from the user. 
+ *
+ * Write a function called multiply that takes both fractions, multiplies
+ * them together, and returns the result as a decimal number. You do not need
+ * to reduce the fraction to its lowest terms.
+ *
+ * Print the result of the multiplication of the 2 fraction variables.
+*/
