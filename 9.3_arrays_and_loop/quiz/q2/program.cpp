5aaa173f76637d1404606f27985b31eb0652328e
jatrindo <jatrindo4096@gmail.com>
2021-05-12 22:11:33 -0500

    Include first draft solution for Q2 of 9.3 Quiz

--- 9.3_arrays_and_loop/quiz/q2/program.cpp
+++ 9.3_arrays_and_loop/quiz/q2/program.cpp
@@ -13,7 +13,76 @@
  *	// if the user entered something invalid
  *	if (std::cin.fail())
  *	{
- *	std::cin.clear();
- *	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer
+ *		std::cin.clear();
+ *		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer
  *	}
 */
+#include <iostream>
+
+int askNumberInRange(const int min, const int max)
+{
+	while (true)
+	{
+		std::cout << "Enter a number between " << min
+			<< " and " << max << ", inclusive: ";
+		int response{};
+		std::cin >> response;
+
+		// If the user pressed ^D, we're assuming they want to exit
+		// the program
+		if (std::cin.eof())
+		{
+			std::exit(0);
+		}
+
+		if (response >= min && response <= max)
+			return response;
+
+		std::cout << "Invalid response.\n";
+
+		// If the extraction failed, clear out the input buffer
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+	}
+}
+
+void printArray(const int array[], const int array_size)
+{
+	for (int i{ 0 }; i < array_size; i++)
+		std::cout << array[i] << ' ';
+	std::cout << '\n';
+}
+
+// Returns index of element, if found. Otherwise returns -1
+int getIndexOfElement(const int element, const int array[], const int array_size)
+{
+	for (int i{ 0 }; i < array_size; i++)
+	{
+		if (array[i] == element)
+			return i;
+	}
+
+	// Return -1 if element was not found
+	return -1;
+}
+
+
+int main()
+{
+	constexpr int array[]{ 4, 6, 7, 3, 8, 2, 1, 9, 5 };
+	constexpr int array_size{ sizeof(array) / sizeof(array[0]) };
+
+	int num{ askNumberInRange(1, 9) };
+
+	// Print the array
+	printArray(array, array_size);
+
+	// Print the index the user's number is at in the array
+	std::cout << "The element '" << num << "' is located at index "
+		  << getIndexOfElement(num, array, array_size) << '\n';
+
+	return 0;
+}
