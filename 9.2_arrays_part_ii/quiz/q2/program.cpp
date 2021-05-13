e5c5b85270c162d4ca9c42ad80c777d82c4b1384
jatrindo <jatrindo4096@gmail.com>
2021-05-12 19:51:38 -0500

    Include initial problem statement and Makefile for Q2 of 9.2 Quiz

---
+++ 9.2_arrays_part_ii/quiz/q2/program.cpp
@@ -0,0 +1,13 @@
+/*
+ * Set up an enum with the names of the following animals: chicken, dog, cat,
+ * elephant, duck, and snake.
+ *
+ * Put the enum in a namespace.
+ *
+ * Define an array with an element for each of the animals, and use an
+ * initializer list to initialize each element to hold the number of legs
+ * that an animal has.
+ *
+ * Write a main function that prints the number of legs an elephant has, using
+ * the enumerator.
+*/
