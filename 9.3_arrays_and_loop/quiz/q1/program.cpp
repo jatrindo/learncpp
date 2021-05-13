8d7c84676e36f1b1ea3a2e3dd78bb2e75cb533d6
jatrindo <jatrindo4096@gmail.com>
2021-05-12 21:27:56 -0500

    Include first draft solution for Q1 of 9.3 Quiz

--- 9.3_arrays_and_loop/quiz/q1/program.cpp
+++ 9.3_arrays_and_loop/quiz/q1/program.cpp
@@ -2,3 +2,17 @@
  * Print the following array to the screen using a loop:
  * 	constexpr int array[]{ 4, 6, 7, 3, 8, 2, 1, 9, 5 };
 */
+#include <iostream>
+#include <iterator>
+
+int main()
+{
+  	constexpr int array[]{ 4, 6, 7, 3, 8, 2, 1, 9, 5 };
+
+	for (int i{ 0 }; i < static_cast<int>(std::size(array)); i++)
+		std::cout << array[i] << ' ';
+
+	std::cout << '\n';
+
+	return 0;
+}
