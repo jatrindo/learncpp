eecd1552f3887400f8a30f9ea732de1c4f57a4fd
jatrindo <jatrindo4096@gmail.com>
2021-05-13 01:07:08 -0500

    Include first draft solution for Q3 of 9.4 Quiz

--- 9.4_sorting_an_array_using_selection_sort/quiz/q3/program.cpp
+++ 9.4_sorting_an_array_using_selection_sort/quiz/q3/program.cpp
@@ -16,3 +16,54 @@
  *
  * Print the sorted array elements at the end of your program.
 */
+#include <iostream>
+#include <utility>
+
+int main()
+{
+	int array[]{ 6, 3, 2, 9, 7, 1, 5, 4, 8 };
+	constexpr std::size_t array_size{ sizeof(array) / sizeof(array[0]) };
+
+	// Strategy:
+	//
+	// On each passthrough of the array, bubble sort manages to move the
+	// largest value in the array to the last index, where it belongs.
+	//
+	// When that happens, only the 0th to size-2 elements remain unsorted,
+	// meaning that we only have to bubble through the [0, size-2] range,
+	// rather than all of [0, size-1].
+	//
+	// Thus with each passthrough of array, the last index of the unsorted
+	// portion of the array decreases by 1
+	//
+	// Once the last unsorted index of the array is 0, we stop since there
+	// are no longer any more elements to swap
+
+	// Initially, the whole array is unsorted, meaning that the last index
+	// of the unsorted portion of the array is exactly the last index of
+	// the array
+	for (std::size_t last_unsorted_index{ array_size - 1 }; last_unsorted_index > 0; last_unsorted_index--)
+	{
+		// Bubble from the beginning to the last index of the unsorted
+		// portion of the array
+		for (std::size_t i{ 0 }; i <= last_unsorted_index; i++)
+		{
+			// Nudge the larger value up if it isn't in the higher
+			// index already
+			if (array[i] > array[i + 1])
+			{
+				std::swap(array[i], array[i + 1]);
+			}
+		}
+	}
+
+	// Print out the now sorted array
+	for (std::size_t i{ 0 }; i < array_size; i++)
+	{
+		std::cout << array[i] << ' ';
+	}
+
+	std::cout << '\n';
+
+	return 0;
+}
