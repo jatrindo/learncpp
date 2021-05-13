b57c438216a0cdb9356393f643d19f5a32b6b99e
jatrindo <jatrindo4096@gmail.com>
2021-05-13 00:41:47 -0500

    Include initial problem statement and Makefile for Q3 of 9.4 Quiz

---
+++ 9.4_sorting_an_array_using_selection_sort/quiz/q3/program.cpp
@@ -0,0 +1,18 @@
+/*
+ * Unoptimized bubble sort performs the following steps to sort an array
+ * from smallest to largest:
+ *
+ * A) Compare array element 0 with array element 1. If element 0 is larger,
+ * swap it with element 1.
+ * B) Now do the same for elements 1 and 2, and every subsequent pair of
+ * elements until you hit the end of the array. At this point the last
+ * element in the array will be sorted.
+ * C) Repeat the first two steps again until the array is sorted.
+ *
+ * Write code that bubble sorts the following array according to the rules
+ * above:
+ *
+ * 	int array[]{ 6, 3, 2, 9, 7, 1, 5, 4, 8 };
+ *
+ * Print the sorted array elements at the end of your program.
+*/
