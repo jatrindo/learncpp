28f3da8ae446d21573651a019e5cd65f01cfa3c0
jatrindo <jatrindo4096@gmail.com>
2021-05-14 09:02:55 -0500

    Include initial problem statement, code, and Makefile for Q2 of Quiz 9.11

---
+++ 9.11_pointer_arithmetic_and_array_indexing/quiz/q2/program.cpp
@@ -0,0 +1,30 @@
+/*
+ * Write a function named find that takes a pointer to the beginning and a
+ * pointer to the end (1 element past the last) of an array, as well as a
+ * value.
+ *
+ * The function should search for the given value and return a pointer to the
+ * first element with that value, or the end pointer if no element was found.
+ *
+ * The following [code in main] should run.
+*/
+#include <iostream>
+#include <iterator>
+
+// ...
+
+int main()
+{
+	int arr[]{ 2, 5, 4, 10, 8, 20, 16, 40 };
+
+	// Search for the first element with value 20.
+	int* found{ find(std::begin(arr), std::end(arr), 20) };
+
+	// If an element with value 20 was found, print it
+	if (found != std::end(arr))
+	{
+		std::cout << *found << '\n';
+	}
+
+	return 0;
+}
