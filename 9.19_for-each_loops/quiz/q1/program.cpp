4e7964e4d7eac104ca42d40d9fdb80fa93f20665
jatrindo <jatrindo4096@gmail.com>
2021-05-15 20:42:01 -0500

    Include first draft solution for Q1 of Quiz 9.19

--- 9.19_for-each_loops/quiz/q1/program.cpp
+++ 9.19_for-each_loops/quiz/q1/program.cpp
@@ -1,6 +1,6 @@
 /*
  * Declare a fixed array with the following names: Alex, Betty, Caroline, Dave,
- * Emily, Fred, Gred, and Holly.
+ * Emily, Fred, Greg, and Holly.
  *
  * Ask the user to enter a name. Use a for each loop to see if the name the
  * user entered is in the array.
@@ -14,3 +14,27 @@
  * 	Enter a name: Megatron
  * 	Megatron was not found.
 */
+#include <iostream>
+
+int main()
+{
+	std::string names[]{ "Alex", "Betty", "Caroline", "Dave", "Emily",
+		"Fred", "Greg", "Holly" };
+
+	std::cout << "Enter a name: ";
+	std::string name_query{};
+	std::cin >> name_query;
+
+	for (const std::string_view name : names)
+	{
+		if (name == name_query)
+		{
+			std::cout << name_query << " was found.\n";
+			return 0;
+		}
+	}
+
+	std::cout << name_query << " was not found.\n";
+
+	return 0;
+}
