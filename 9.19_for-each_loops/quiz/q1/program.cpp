40472e7cc121c04c399fcd991f66503f14ee6202
jatrindo <jatrindo4096@gmail.com>
2021-05-15 20:34:23 -0500

    Include initial problem statement and Makefile for Q1 of 9.19 Quiz

---
+++ 9.19_for-each_loops/quiz/q1/program.cpp
@@ -0,0 +1,16 @@
+/*
+ * Declare a fixed array with the following names: Alex, Betty, Caroline, Dave,
+ * Emily, Fred, Gred, and Holly.
+ *
+ * Ask the user to enter a name. Use a for each loop to see if the name the
+ * user entered is in the array.
+ *
+ * Sample output:
+ *
+ * 	Enter a name: Betty
+ * 	Betty was found.
+ *
+ *
+ * 	Enter a name: Megatron
+ * 	Megatron was not found.
+*/
