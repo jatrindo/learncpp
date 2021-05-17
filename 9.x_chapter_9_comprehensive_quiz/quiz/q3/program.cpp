c57664c9c65be0014eb6bac00374d584dabf7165
jatrindo <jatrindo4096@gmail.com>
2021-05-17 08:48:28 -0500

    Include first draft solution for Q3 of 9.x Quiz

--- 9.x_chapter_9_comprehensive_quiz/quiz/q3/program.cpp
+++ 9.x_chapter_9_comprehensive_quiz/quiz/q3/program.cpp
@@ -3,3 +3,29 @@
  *
  * Write a main() function to test it.
 */
+#include <iostream>
+
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+int main()
+{
+	std::cout << "Testing swap....\n";
+
+	int a{ 5 };
+	int b{ 7 };
+
+	std::cout << "Before:\n";
+	std::cout << "a = " << a << ", b = " << b << '\n';
+
+	swap(a, b);
+
+	std::cout << "After:\n";
+	std::cout << "a = " << a << ", b = " << b << '\n';
+
+	return 0;
+}
