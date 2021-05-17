70719c9ef36f0771c6b7b1c8a968de6e14dbb920
jatrindo <jatrindo4096@gmail.com>
2021-05-16 20:29:27 -0500

    Include initial problem statement and Makefile for Q1 of 9.x Quiz

---
+++ 9.x_chapter_9_comprehensive_quiz/quiz/q1/program.cpp
@@ -0,0 +1,15 @@
+/*
+ * Pretend you're writing a game where the player can hold 3 types of items:
+ * health potions, torches, and arrows.
+ *
+ * Create an enum to identify the different types of items, and a std::array
+ * to store the number of each item the player is carrying (The enumerators
+ * are used as indexes of the array).
+ * The player should start with 2 health potions, 5 torches, and 10 arrows.
+ *
+ * Write a function called countTotalItems() that returns how many items the
+ * player has in total.
+ *
+ * Have your main() function print the output of countTotalItems() as well as
+ * the number of torches.
+*/
