9995582b5e70936a1ede32a16a703f9dd6dab227
jatrindo <jatrindo4096@gmail.com>
2021-05-16 21:12:25 -0500

    Include initial problem statement and Makefile for Q2 of Quiz 9.x

---
+++ 9.x_chapter_9_comprehensive_quiz/quiz/q2/program.cpp
@@ -0,0 +1,37 @@
+/*
+ * Write the following program:
+ *
+ * Create a struct that holds a student's first name and grade (on a scale
+ * of 0-100).
+ *
+ * Ask the user how many students they want to enter. Create a std::vector to
+ * hold all of the sutdents. Then prompt the user for each name and grade.
+ *
+ * Once the user has entered all the names and grade pairs sort the list by
+ * grade (highest first).
+ *
+ * Then print all the names and grades in sorted order.
+ *
+ * For the following input:
+ *
+ * 	Joe
+ * 	82
+ * 	Terry
+ * 	73
+ * 	Ralph
+ * 	4
+ * 	Alex
+ * 	94
+ * 	Mark
+ * 	88
+ *
+ * The output should look like this:
+ * 
+ * 	Alex got a grade of 94
+ * 	Mark got a grade of 88
+ * 	Joe got a grade of 82
+ * 	Terry got a grade of 73
+ * 	Ralph got a grade of 4
+ *
+ * You can assume that names don't contain spaces and that input extraction doesn't fail.
+*/
