3d1dce48e1a7f6fe782b9c568723bf441a32e2ec
jatrindo <jatrindo4096@gmail.com>
2021-05-16 20:56:26 -0500

    Include first draft solution for Q1 of 9.x Quiz

--- 9.x_chapter_9_comprehensive_quiz/quiz/q1/program.cpp
+++ 9.x_chapter_9_comprehensive_quiz/quiz/q1/program.cpp
@@ -13,3 +13,48 @@
  * Have your main() function print the output of countTotalItems() as well as
  * the number of torches.
 */
+#include <iostream>
+#include <array>
+#include <numeric> // for std::reduce/std::accumulate
+
+// We're using enums as array indexes, so we decalre the enums inside a
+// namespace rather than declare an enum class.
+//
+// Otherwise we'd have to deal with a lot of static_casts everywhere since
+// enums within an enum class aren't implicitly converted into integers by
+// the compiler.
+//
+// Reference: learncpp.com, chapter 9.2
+namespace Items
+{
+	enum
+	{
+		health_potion,
+		torches,
+		arrows,
+
+		max_items
+	};
+}
+
+int countTotalItems(std::array<int, Items::max_items>& inventory)
+{
+	// std::reduce not available, fall back to std::accumulate
+	return std::accumulate(inventory.begin(), inventory.end(), 0);
+}
+
+int main()
+{
+	std::array<int, Items::max_items> inventory{};
+	inventory[Items::health_potion] = 2;
+	inventory[Items::torches] = 5;
+	inventory[Items::arrows] = 10;
+
+	std::cout << "Number of total items: "
+		  << countTotalItems(inventory) << '\n';
+
+	std::cout << "Number of torches: "
+		  << inventory[Items::torches] << '\n';
+
+	return 0;
+}
