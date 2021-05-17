dff3f1aff681861298b4a3d0d019c9f2c4f7c443
jatrindo <jatrindo4096@gmail.com>
2021-05-17 11:10:59 -0500

    Include first draft solution for part e) of Q6 of 9.x Quiz, with a correction made in part c), and a tweak in part d) to more closely adhere to the output format

--- 9.x_chapter_9_comprehensive_quiz/quiz/q6/program.cpp
+++ 9.x_chapter_9_comprehensive_quiz/quiz/q6/program.cpp
@@ -120,7 +120,7 @@ void printCard(const Card& card)
 			rank_code = 'J';
 			break;
 		case CardRank::queen:
-			rank_code = 'A';
+			rank_code = 'Q';
 			break;
 		case CardRank::king:
 			rank_code = 'K';
@@ -172,9 +172,9 @@ DeckInfo::deck_t createDeck()
 	int num_ranks{ static_cast<int>(CardRank::max_ranks) };
 	int num_suites{ static_cast<int>(CardSuit::max_suits) };
 
-	for (int rank{ 0 }; rank < num_ranks; ++rank)
+	for (int suit{ 0 }; suit < num_suites; ++suit)
 	{
-		for (int suit{ 0 }; suit < num_suites; ++suit)
+		for (int rank{ 0 }; rank < num_ranks; ++rank)
 		{
 			deck[card_index].suit = static_cast<CardSuit>(suit);
 			deck[card_index].rank = static_cast<CardRank>(rank);
@@ -185,8 +185,23 @@ DeckInfo::deck_t createDeck()
 	return deck;
 }
 // Answer for d) -- End
+
+// Answer for e) -- Start
+void printDeck(const DeckInfo::deck_t& deck)
+{
+	for (const Card& card: deck)
+	{
+		printCard(card);
+		std::cout << ' ';
+	}
+	std::cout << '\n';
+}
+// Answer for e) -- End
+
 int main()
 {
 	auto deck{ createDeck() };
+	printDeck(deck);
+
 	return 0;
 }
