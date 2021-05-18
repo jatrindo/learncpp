6932f15e82899f40bd2fe360958058448f390217
jatrindo <jatrindo4096@gmail.com>
2021-05-17 22:09:19 -0500

    Include initial problem statement and Makefile for Q7 of 9.x Quiz

---
+++ 9.x_chapter_9_comprehensive_quiz/quiz/q7/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Alright, challenge time! Let's write a simplified version of Blackjack.
+ *
+ * (...Blackjack rules explanation...)
+ *
+ * In our simplified version of Blackjack, we're not going to keep track of
+ * which specific cards that player and dealer have dealt. We'll only track
+ * the sum of the values of the cards they have been dealt for the player and
+ * dealer. This keeps things simpler.
+ *
+ * Start with the code you wrote in quiz #6. Create a function named
+ * playBlackjack(). The function should:
+ * 
+ * 	- Accept a shuffled deck of cards as a parameter.
+ * 	- Implement Blackjack as defined above.
+ * 	- Returns true if the player won, and false if they lost.
+ *
+ * Also, write a main() function to play a single game of Blackjack.
+*/
