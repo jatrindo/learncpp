63b8d9cbc691009042e5d38667e431ede29de781
jatrindo <jatrindo4096@gmail.com>
2021-05-24 23:37:14 -0500

    Include initial problem statement and Makefile for Q1 of 10.12 Quiz

---
+++ 10.12_recursion/quiz/q1/program.cpp
@@ -0,0 +1,7 @@
+/*
+ * A factorial of an integer N (written N!) is defined as the product
+ * (multiplation) of all the numbers between 1 and N (0! =1).
+ *
+ * Write a recursive function called factorial that returns the factorial of
+ * the input. Test it with the first 7 factorials
+*/
