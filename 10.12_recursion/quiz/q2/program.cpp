8023f6c3586af07f5c1d24d6af2272fd6fc66bf2
jatrindo <jatrindo4096@gmail.com>
2021-05-24 23:52:36 -0500

    Include first draft solution for Q2 of 10.12 Quiz

--- 10.12_recursion/quiz/q2/program.cpp
+++ 10.12_recursion/quiz/q2/program.cpp
@@ -5,3 +5,20 @@
  * Print the answer for input 93427 (which is 25). Assume the input values are
  * positive.
 */
+#include <iostream>
+
+int sum_of_digits(int n)
+{
+	if (n <= 0)
+		return 0;
+	return sum_of_digits(n / 10) + (n % 10);
+}
+
+int main()
+{
+	const int n{ 93427 };
+	std::cout << "Sum of the digits of " << n << " is "
+		  << sum_of_digits(n) << '\n';
+
+	return 0;
+}
