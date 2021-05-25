c9d1af0d3a7a61ffdd4ec7a06862285b3c5bfd02
jatrindo <jatrindo4096@gmail.com>
2021-05-24 23:56:12 -0500

    Include initial problem statement and Makefile for Q3 of 10.12 Quiz

---
+++ 10.12_recursion/quiz/q3/program.cpp
@@ -0,0 +1,6 @@
+/*
+ * Write a program tat asks the user to enter a positive integer, and then
+ * uses a recursive function to print out the binary representation for that
+ * number. Use method 1 from lesson O.4 -- Converting between binary and
+ * decimal
+*/
