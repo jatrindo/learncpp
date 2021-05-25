26406fbdefdde941d52cf2deb045ea3593cdc633
jatrindo <jatrindo4096@gmail.com>
2021-05-24 23:44:59 -0500

    Include initial problem statement and Makefile for Q2 of 10.12 Quiz

---
+++ 10.12_recursion/quiz/q2/program.cpp
@@ -0,0 +1,7 @@
+/*
+ * Write a recursive function that takes an integer as input and returns the
+ * sum of each individual digit in the integer (e.g 357 = 3 + 5 + 7 = 15).
+ *
+ * Print the answer for input 93427 (which is 25). Assume the input values are
+ * positive.
+*/
