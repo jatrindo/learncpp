d858122b88db37eb56966e94e072ae0cbefdc884
jatrindo <jatrindo4096@gmail.com>
2021-05-24 23:42:21 -0500

    Include first draft solution for Q1 of 10.12 Quiz

--- 10.12_recursion/quiz/q1/program.cpp
+++ 10.12_recursion/quiz/q1/program.cpp
@@ -5,3 +5,20 @@
  * Write a recursive function called factorial that returns the factorial of
  * the input. Test it with the first 7 factorials
 */
+#include <iostream>
+
+int factorial(int n)
+{
+	if (n == 0)
+		return 1;
+
+	return factorial(n - 1) * n;
+}
+
+int main()
+{
+	for (int i{ 0 }; i < 7; ++i)
+		std::cout << "factorial(" << i << ") is " << factorial(i) << '\n';
+
+	return 0;
+}
