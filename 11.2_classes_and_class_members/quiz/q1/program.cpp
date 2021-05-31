96c498781fd091a05ef55839e00052bac206558b
jatrindo <jatrindo4096@gmail.com>
2021-05-30 19:57:33 -0500

    Include initial problem statement, code, and Makefile for Q1 of 11.2 Quiz

---
+++ 11.2_classes_and_class_members/quiz/q1/program.cpp
@@ -0,0 +1,23 @@
+/*
+ * Create a class called IntPair that holds two integers.
+ *
+ * This class should have two member variables to hold the integers.
+ *
+ * You should also create two member functions: one names "set" that will let
+ * you assign values to the integers, and one named "print" that will print the
+ * values of the variables.
+ *
+ * The following main function should execute:
+*/
+int main()
+{
+	IntPair p1;
+	p1.set(1, 1); // set p1 values to (1, 1)
+
+	IntPair p2{ 2, 2 }; // initialize p2 values to (2, 2)
+
+	p1.print();
+	p2.print();
+
+	return 0;
+}
