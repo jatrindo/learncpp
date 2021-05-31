1129475112e05e4bab24978e8f43ef8bdf069327
jatrindo <jatrindo4096@gmail.com>
2021-05-30 23:58:58 -0500

    Include initial problem statement and code for part b) of Q2 of 11.3 Quiz

--- 11.3_public_vs_private_access_specifiers/quiz/q2/program.cpp
+++ 11.3_public_vs_private_access_specifiers/quiz/q2/program.cpp
@@ -1,5 +1,5 @@
 /*
- * Write a simple class named Point3d. The class should contain:
+ * a) Write a simple class named Point3d. The class should contain:
  *
  * 	- Three private member variables of type int named m_x, m_y, m_z
  * 	- A public member function named setValues() that allows you to set
@@ -10,6 +10,9 @@
  * Make sure the following main function executes correctly. This should print:
  *
  * 	<1, 2, 3>
+ *
+ * b) Add a function named isEqual() to your Point3d class. The following
+ * (upadted main()) code should run correctly.
 */
 #include <iostream>
 
@@ -35,11 +38,42 @@ public:
 
 int main()
 {
+	// Code for part a)
+	/*
 	Point3d point;
 	point.setValues(1, 2, 3);
 
 	point.print();
 	std::cout << '\n';
+	*/
+
+	// Code for part b)
+	Point3d point1;
+	point1.setValues(1, 2, 3);
+
+	Point3d point2;
+	point2.setValues(1, 2, 3);
+
+	if (point1.isEqual(point2))
+	{
+		std::cout << "point1 and point2 are equal\n";
+	}
+	else
+	{
+		std::cout << "point1 and point2 are not equal\n";
+	}
+
+	Point3d point3;
+	point3.setValues(3, 4, 5);
+
+	if (point1.isEqual(point3))
+	{
+		std::cout << "point1 and point2 are equal\n";
+	}
+	else
+	{
+		std::cout << "point1 and point2 are not equal\n";
+	}
 
 	return 0;
 }
