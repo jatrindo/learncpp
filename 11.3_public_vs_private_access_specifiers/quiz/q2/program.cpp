fdc5ab678777fd8719b507c6ab796b149c1d7081
jatrindo <jatrindo4096@gmail.com>
2021-05-30 23:53:01 -0500

    Include first draft solution for part a) of Q2 of 11.3 Quiz

--- 11.3_public_vs_private_access_specifiers/quiz/q2/program.cpp
+++ 11.3_public_vs_private_access_specifiers/quiz/q2/program.cpp
@@ -11,6 +11,28 @@
  *
  * 	<1, 2, 3>
 */
+#include <iostream>
+
+class Point3d
+{
+	int m_x{};
+	int m_y{};
+	int m_z{};
+
+public:
+	void setValues(int x, int y, int z)
+	{
+		m_x = x;
+		m_y = y;
+		m_z = z;
+	}
+
+	void print()
+	{
+		std::cout << '<' << m_x << ", " << m_y << ", " << m_z << '>';
+	}
+};
+
 int main()
 {
 	Point3d point;
