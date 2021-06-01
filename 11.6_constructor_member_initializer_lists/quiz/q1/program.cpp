093f3577818596dd433895675fa99ea4a7db6202
jatrindo <jatrindo4096@gmail.com>
2021-05-31 20:49:30 -0500

    Include first draft solution for Q1 of 11.6 Quiz

--- 11.6_constructor_member_initializer_lists/quiz/q1/program.cpp
+++ 11.6_constructor_member_initializer_lists/quiz/q1/program.cpp
@@ -14,6 +14,34 @@
  *
  * 	r=0 g=127 b=127 a=255
 */
+#include <iostream>
+#include <cstdint>
+
+class RGBA
+{
+public:
+	using color_t = std::uint_fast8_t;
+
+private:
+	color_t m_red;
+	color_t m_green;
+	color_t m_blue;
+	color_t m_alpha;
+
+public:
+	RGBA(color_t red=0, color_t green=0, color_t blue=0, color_t alpha=255)
+		: m_red{ red }, m_green{ green }, m_blue{ blue },  m_alpha{ alpha }
+	{} // Empty constructor body
+
+	void print()
+	{
+		std::cout << "r=" << static_cast<int>(m_red) << ' '
+			  << "g=" << static_cast<int>(m_green) << ' '
+			  << "b=" << static_cast<int>(m_blue) << ' '
+			  << "a=" << static_cast<int>(m_alpha) << '\n';
+	}
+};
+
 int main()
 {
 	RGBA teal{ 0, 127, 127 };
