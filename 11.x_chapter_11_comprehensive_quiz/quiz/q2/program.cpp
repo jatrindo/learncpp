9c48e0003900b8ded1a15ef0f04767ab9fa173ab
jatrindo <jatrindo4096@gmail.com>
2021-06-05 12:02:02 -0500

    Include initial problem statement, code, and Makefile for Q2 of 11.x Quiz

---
+++ 11.x_chapter_11_comprehensive_quiz/quiz/q2/program.cpp
@@ -0,0 +1,37 @@
+/*
+ * Write a destructor for this class:
+*/
+#include <iostream>
+
+class HelloWorld
+{
+private:
+	char *m_data{};
+
+public:
+	HelloWorld()
+	{
+		m_data = new char[14];
+		const char *init{ "Hello, World!" };
+		for (int i = 0; i < 14; ++i)
+			m_data[i] = init[i];
+	}
+
+	~HelloWorld()
+	{
+		// replace this comment with your destructor implementation
+	}
+
+	void print() const
+	{
+		std::cout << m_data << '\n';
+	}
+};
+
+int main()
+{
+	HelloWorld hello{};
+	hello.print();
+
+	return 0;
+}
