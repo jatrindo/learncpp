db2a65b352fc7195647fcf0e282e4d95a0c2d68b
jatrindo <jatrindo4096@gmail.com>
2021-06-05 11:38:24 -0500

    Tweak first draft solution for part a) of Q1 of 11.x Quiz, after referencing solution code

--- 11.x_chapter_11_comprehensive_quiz/quiz/q1/program.cpp
+++ 11.x_chapter_11_comprehensive_quiz/quiz/q1/program.cpp
@@ -44,16 +44,17 @@
 
 class Point2d
 {
+private:
 	double m_x{};
 	double m_y{};
 
 public:
-	Point2d(double x = 0, double y = 0)
+	Point2d(double x = 0.0, double y = 0.0)
 		: m_x{ x }, m_y{ y }
 	{
 	}
 
-	void print()
+	void print() const
 	{
 		std::cout << "Point2d(" << m_x << ", " << m_y << ")\n";
 	}
