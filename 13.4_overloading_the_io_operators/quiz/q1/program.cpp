46cc0c53a0768dc1a55b8b916e3affb82fb2b5f5
jatrindo <jatrindo4096@gmail.com>
2021-06-06 18:06:24 -0500

    Include initial problem statement, code, and Makefile for Q1 of Quiz 13.4

---
+++ 13.4_overloading_the_io_operators/quiz/q1/program.cpp
@@ -0,0 +1,86 @@
+/*
+ *
+ * Take the Fraction class we wrote in the prvious quiz ([Listing A]) and add an
+ * overloaded operator<< and operator>> to it.
+ *
+ * The following program should compile: [Listing M]
+ *
+ * And produce the result:
+ *
+ *  Enter fraction 1: 2/3
+ *  Enter fraction 2: 3/8
+ *  2/3 * 3/8 is 1/4
+*/
+
+// Listing A
+#include <iostream>
+ 
+class Fraction
+{
+private:
+	int m_numerator{};
+	int m_denominator{};
+ 
+public:
+	Fraction(int numerator=0, int denominator=1):
+		m_numerator{numerator}, m_denominator{denominator}
+	{
+		// We put reduce() in the constructor to ensure any new fractions we make get reduced!
+		// Any fractions that are overwritten will need to be re-reduced
+		reduce();
+	}
+ 
+	// We'll make gcd static so that it can be part of class Fraction without requiring an object of type Fraction to use
+	static int gcd(int a, int b)
+	{
+		return b == 0 ? a : gcd(b, a % b);
+	}
+ 
+	void reduce()
+	{
+		if (m_numerator != 0 && m_denominator != 0)
+		{
+			int gcd{ Fraction::gcd(m_numerator, m_denominator) };
+			m_numerator /= gcd;
+			m_denominator /= gcd;
+		}
+	}
+ 
+	friend Fraction operator*(const Fraction &f1, const Fraction &f2);
+	friend Fraction operator*(const Fraction &f1, int value);
+	friend Fraction operator*(int value, const Fraction &f1);
+ 
+	void print() const
+	{
+		std::cout << m_numerator << '/' << m_denominator << '\n';
+	}
+};
+ 
+Fraction operator*(const Fraction &f1, const Fraction &f2)
+{
+	return Fraction(f1.m_numerator * f2.m_numerator, f1.m_denominator * f2.m_denominator);
+}
+ 
+Fraction operator*(const Fraction &f1, int value)
+{
+	return Fraction(f1.m_numerator * value, f1.m_denominator);
+}
+ 
+Fraction operator*(int value, const Fraction &f1)
+{
+	return Fraction(f1.m_numerator * value, f1.m_denominator);
+}
+
+// Listing M
+int main()
+{
+    Fraction f1{};
+    std::cout << "Enter fraction 1: ";
+    std::cin >> f1;
+
+    Fraction f2{};
+    std::cout << "Enter fraction 2: ";
+    std::cin >> f2;
+
+    std::cout << f1 << " * " << f2 << " is " << f1 * f2 << '\n'; // note The result of f1 * f2 is an r-value
+}
