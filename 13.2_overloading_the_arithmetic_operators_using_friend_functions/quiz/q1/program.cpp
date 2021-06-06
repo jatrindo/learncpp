ebb650e54a3b40d640c20fb21b6be69fc35553b1
jatrindo <jatrindo4096@gmail.com>
2021-06-05 22:33:50 -0500

    Include first draft solution for part b) of Q1 of Quiz 13.2

--- 13.2_overloading_the_arithmetic_operators_using_friend_functions/quiz/q1/program.cpp
+++ 13.2_overloading_the_arithmetic_operators_using_friend_functions/quiz/q1/program.cpp
@@ -73,45 +73,64 @@ public:
     {
         std::cout << m_num << '/' << m_den << '\n';
     }
+
+    friend Fraction operator*(const Fraction& frac, int i);
+    friend Fraction operator*(int i, const Fraction& frac);
+    friend Fraction operator*(const Fraction& frac1, const Fraction& frac2);
 };
 
+Fraction operator*(const Fraction &frac, int i)
+{
+    return Fraction{ frac.m_num * i, frac.m_den };
+}
+
+Fraction operator*(int i, const Fraction &frac)
+{
+    return { frac * i };
+}
+
+Fraction operator*(const Fraction &frac1, const Fraction &frac2)
+{
+    return Fraction{ frac1.m_num * frac2.m_num, frac1.m_den * frac2.m_den };
+}
+
 // Listing A
+//int main()
+//{
+//    Fraction f1{ 1, 4 };
+//    f1.print();
+//
+//    Fraction f2{ 1, 2 };
+//    f2.print();
+//
+//    return 0;
+//}
+
+// Listing B
+#include <iostream>
+ 
 int main()
 {
-    Fraction f1{ 1, 4 };
+    Fraction f1{2, 5};
     f1.print();
  
-    Fraction f2{ 1, 2 };
+    Fraction f2{3, 8};
     f2.print();
+ 
+    Fraction f3{ f1 * f2 };
+    f3.print();
+ 
+    Fraction f4{ f1 * 2 };
+    f4.print();
+ 
+    Fraction f5{ 2 * f2 };
+    f5.print();
+ 
+    Fraction f6{ Fraction{1, 2} * Fraction{2, 3} * Fraction{3, 4} };
+    f6.print();
  
     return 0;
 }
-
-// Listing B
-// #include <iostream>
- 
-// int main()
-// {
-//     Fraction f1{2, 5};
-//     f1.print();
- 
-//     Fraction f2{3, 8};
-//     f2.print();
- 
-//     Fraction f3{ f1 * f2 };
-//     f3.print();
- 
-//     Fraction f4{ f1 * 2 };
-//     f4.print();
- 
-//     Fraction f5{ 2 * f2 };
-//     f5.print();
- 
-//     Fraction f6{ Fraction{1, 2} * Fraction{2, 3} * Fraction{3, 4} };
-//     f6.print();
- 
-//     return 0;
-// }
 
 // Listing E
 // #include <iostream>
