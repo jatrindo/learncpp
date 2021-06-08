5c0b7a9f2d0212b0854341f2901d4a6a8abff7f7
jatrindo <jatrindo4096@gmail.com>
2021-06-08 00:09:08 -0500

    Include first draft solution for Q2 of Quiz 13.7

--- 13.7_overloading_the_comparison_operators/quiz/q2/program.cpp
+++ 13.7_overloading_the_comparison_operators/quiz/q2/program.cpp
@@ -29,6 +29,10 @@ public:
 
     friend bool operator==(const Car& c1, const Car& c2);
     friend bool operator!=(const Car& c1, const Car& c2);
+
+    friend std::ostream& operator<<(std::ostream& in, const Car& car);
+
+    friend bool operator<(const Car& c1, const Car& c2);
 };
 
 bool operator==(const Car& c1, const Car& c2)
@@ -40,6 +44,22 @@ bool operator==(const Car& c1, const Car& c2)
 bool operator!=(const Car& c1, const Car& c2)
 {
     return !(c1 == c2);
+}
+
+std::ostream &operator<<(std::ostream &out, const Car &car)
+{
+    out << '(' << car.m_make << ", " << car.m_model << ")";
+    return out;
+}
+
+bool operator<(const Car &c1, const Car &c2)
+{
+    // If the names of the make are equal, then the model name tie-breaks
+    if (c1.m_make == c2.m_make)
+        return (c1.m_model < c2.m_model);
+
+    // Otherwise we just compare the names of the make only
+    return (c1.m_make < c2.m_make);
 }
 
 // Listing M
