b955ab26b43fe98d6395002835164173cddad9a0
jatrindo <jatrindo4096@gmail.com>
2021-06-12 22:58:14 -0500

    Include first draft solution for Q3 of Quiz 13.x

--- 13.x_chapter_13_comprehensive_quiz/quiz/q3/program.cpp
+++ 13.x_chapter_13_comprehensive_quiz/quiz/q3/program.cpp
@@ -2,7 +2,6 @@
  * Write your own integer array class named intArray from scratch (do not use
  * std::array or std::vector).
  *
- *
  * Users should pass in the size of the array when it is created, and the array
  * should be dynamically allocated. Use assert statements to guard against bad
  * data.
@@ -19,6 +18,91 @@
 
 // Listing M
 #include <iostream>
+#include <cassert>
+
+class IntArray
+{
+private:
+    int m_length{ 0 };
+    int* m_array{ nullptr };
+
+public:
+    explicit IntArray(int length)
+    {
+        assert(length >= 0 && "IntArray() - Non-positive intial length given");
+
+        m_length = length;
+        m_array = new int[length];
+    }
+
+    IntArray(const IntArray& intarr)
+    {
+        // Self-assignment guard
+        if (&intarr == this)
+            return;
+
+        assert(intarr.m_length >= 0 && "IntArray(const IntArray&) -"
+            " IntArray attempted to be constructed with IntArray of non-positive length");
+
+        // Delete the previous array we have to prevent memory leakage
+        delete[] m_array;
+
+        // Update length and pointer
+        m_length = intarr.m_length;
+        m_array = new int[intarr.m_length];
+
+        // Copy the parameter's array into the new array
+        for (int i{ 0 }; i < intarr.m_length; ++i)
+        {
+            m_array[i] = intarr.m_array[i];
+        }
+    }
+
+    ~IntArray()
+    {
+        delete[] m_array;
+    }
+
+    int& operator[](int i)
+    {
+        assert( i >= 0 && i < m_length && "IntArray::operator[] - Invalid index given");
+        return m_array[i];
+    }
+
+    IntArray& operator=(const IntArray& intarr)
+    {
+        // Self-assignment guard
+        if (&intarr == this)
+            return *this;
+
+        assert(intarr.m_length >= 0 && "IntArray::operator= -"
+            " IntArray with non-positive length attribute attempted to be assigned");
+
+        // Delete the previous array we have to prevent memory leakage
+        delete[] m_array;
+
+        // Update length and pointer
+        m_length = intarr.m_length;
+        m_array = new int[intarr.m_length];
+
+        // Copy the parameter's array into the new array
+        for (int i{ 0 }; i < intarr.m_length; ++i)
+        {
+            m_array[i] = intarr.m_array[i];
+        }
+
+        return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const IntArray& arr)
+    {
+        for (int i{ 0 }; i < arr.m_length; ++i)
+        {
+            out << arr.m_array[i] << ' ';
+        }
+        return out;
+    }
+};
 
 IntArray fillArray()
 {
@@ -30,7 +114,7 @@ IntArray fillArray()
     a[3] = 3;
     a[4] = 6;
 
-    return 0;
+    return a;
 }
 
 int main()
