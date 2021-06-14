87c3d762253d793b2f188823b44433fe6bd9137b
jatrindo <jatrindo4096@gmail.com>
2021-06-14 09:33:23 -0500

    Include initial problem statement, code, and Makefile for Q2 of Quiz 16.3

---
+++ 16.3_aggregation/quiz/q2/program.cpp
@@ -0,0 +1,63 @@
+/*
+ * Update the Department/Teacher example [Listing E] so the Department can
+ * handle multiple Teachers.
+ * 
+ * The following code should execute: [Listing M]
+ * 
+*/
+#include <iostream>
+#include <string>
+
+// Listing E
+class Teacher
+{
+private:
+    std::string m_name{};
+
+public:
+    Teacher(const std::string& name)
+        : m_name{ name }
+    {
+    }
+
+    const std::string& getName() const { return m_name };
+};
+
+class Department
+{
+private:
+    const Teacher& m_teacher; // This dept holds only one teacher for simplicity, but it could hold many teachers
+
+public:
+    Department(const Teacher& teacher)
+        : m_teacher{ teacher }
+    {
+    }
+};
+
+// Listing M
+int main()
+{
+    // Create a teacher outside the scope of the Department
+    Teacher t1{ "Bob" };
+    Teacher t2{ "Frank" };
+    Teacher t3{ "Beth" };
+
+    {
+        // Create a department and add some Teachers to it
+        Department department{}; // Create an empty Department
+
+        department.add(t1);
+        department.add(t2);
+        department.add(t3);
+
+        std::cout << department;
+
+    } // department goes out of scope here and is destroyed
+
+    std::cout << t1.getName() << " still exists!\n";
+    std::cout << t2.getName() << " still exists!\n";
+    std::cout << t3.getName() << " still exists!\n";
+
+    return 0;
+}
