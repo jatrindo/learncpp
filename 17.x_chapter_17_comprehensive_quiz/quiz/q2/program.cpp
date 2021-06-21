39866042dc16e1d2297e29d29768400b2ad9a8ae
jatrindo <jatrindo4096@gmail.com>
2021-06-20 22:20:59 -0500

    Include problem statement and initial code for part b) of Q2 of Quiz 17.x

--- 17.x_chapter_17_comprehensive_quiz/quiz/q2/program.cpp
+++ 17.x_chapter_17_comprehensive_quiz/quiz/q2/program.cpp
@@ -1,15 +1,26 @@
 /*
- * Write an Apple class and a Banana class that are derived from a common Fruit class.
+ * a) Write an Apple class and a Banana class that are derived from a common Fruit class.
  *
  * Fruit should have two members: a name, and a color.
  * 
- * The following program should run: [Listing M]
+ * The following program should run: [Listing M1]
  * 
  * And produce the result:
  * 
  *  My apple is red.
  *  My banana is yellow.
  * 
+ *
+ * b) Add a new class to the prvious program called GrannySmith that inherits from Apple
+ *
+ * The following program should run: [Listng M2]
+ *
+ * And produce the result:
+ *
+ *  My apple is red.
+ *  My banana is yellow.
+ *  My granny smith apple is green.
+ *
  */
 #include <iostream>
 #include <string>
@@ -48,14 +59,28 @@ public:
     }
 };
 
-// Listing M
+// Listing M1
+//int main()
+//{
+//    Apple a{ "red" };
+//    Banana b{};
+//
+//    std::cout << "My " << a.getName() << " is " << a.getColor() << ".\n";
+//    std::cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
+//
+//    return 0;
+//}
+
+// Listng M2
 int main()
 {
-    Apple a{ "red" };
-    Banana b{};
+    Apple a{ "red " };
+    Banana b;
+    GrannySmith c;
 
     std::cout << "My " << a.getName() << " is " << a.getColor() << ".\n";
     std::cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
+    std::cout << "My " << c.getName() << " is " << c.getColor() << ".\n";
 
     return 0;
 }
