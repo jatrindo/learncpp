d6c6030ba4dc477a643af1271ca9c4286450b182
jatrindo <jatrindo4096@gmail.com>
2021-07-07 20:57:13 -0500

    Include first draft solution for part b) of Q2 of Quiz 18.x

--- 18.x_chapter_18_comprehensive_quiz/quiz/q2/program.cpp
+++ 18.x_chapter_18_comprehensive_quiz/quiz/q2/program.cpp
@@ -40,37 +40,76 @@ public:
 };
 
 // Listing BP
-//class Point
-//{
-//private:
-//    int m_x{};
-//    int m_y{};
-//    int m_z{};
-//
-//public:
-//    Point(int x, int y, int z)
-//        : m_x{x}, m_y{y}, m_z{z}
-//    {
-//    }
-//
-//    friend std::ostream& operator<<(std::ostream& out, const Point& p)
-//    {
-//        out << "Point(" << p.m_x << ", " << p.m_y << ", " << p.m_z << ')';
-//        return out;
-//    }
-//}
+class Point
+{
+private:
+    int m_x{};
+    int m_y{};
+    int m_z{};
+
+public:
+    Point(int x, int y, int z)
+        : m_x{x}, m_y{y}, m_z{z}
+    {
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Point& p)
+    {
+        out << "Point(" << p.m_x << ", " << p.m_y << ", " << p.m_z << ')';
+        return out;
+    }
+};
+
+class Triangle: public Shape
+{
+private:
+    Point m_p1;
+    Point m_p2;
+    Point m_p3;
+
+public:
+    Triangle(Point p1, Point p2, Point p3)
+        : m_p1{p1}, m_p2{p2}, m_p3{p3}
+    {
+    }
+
+    virtual std::ostream& print(std::ostream& out) const
+    {
+        out << "Triangle(" << m_p1 << ", " << m_p2 << ", " << m_p3 << ')';
+        return out;
+    }
+};
+
+class Circle: public Shape
+{
+private:
+    Point m_center;
+    int m_radius;
+
+public:
+    Circle(Point center, int radius)
+        : m_center{ center }, m_radius{ radius }
+    {
+    }
+
+    virtual std::ostream& print(std::ostream& out) const
+    {
+        out << "Circle(" << m_center << ", radius " << m_radius << ')';
+        return out;
+    }
+};
 
 // Listing B
-//int main()
-//{
-//    Circle c{ Point{ 1, 2, 3}, 7 };
-//    std::cout << c << '\n';
-//
-//    Triangle t{ Point{ 1, 2, 3}, Point{ 4, 5, 6}, Point{ 7, 8, 9}};
-//    std::cout << t << '\n';
-//
-//    return 0;
-//}
+int main()
+{
+    Circle c{ Point{ 1, 2, 3}, 7 };
+    std::cout << c << '\n';
+
+    Triangle t{ Point{ 1, 2, 3}, Point{ 4, 5, 6}, Point{ 7, 8, 9}};
+    std::cout << t << '\n';
+
+    return 0;
+}
 
 // Listing C
 //#include <vector>
