4e617f2b0120be9035e8771453db145a177c70eb
jatrindo <jatrindo4096@gmail.com>
2021-07-10 13:38:28 -0500

    Include problem statement, initial code, and Makefile for Q3 of Quiz 19.x

---
+++ 19.x_chapter_19_comprehensive_quiz/quiz/q3/program.cpp
@@ -0,0 +1,40 @@
+/*
+ * A string-value pair is a special type of pair where the first value is always
+ * a string type, and the second value can be any type.
+ *
+ * Write a template class named StringValuePair that inherits from a partially
+ * specialized Pair class (using std::string as the first type, and allowing the
+ * user to specify the second type).
+ * 
+ * The following program should run: [List M]
+ *
+ * and print:
+ * 
+ *  Pair: Hello 5
+*/
+// Pair class from Q2:
+template <typename T, typename U>
+class Pair
+{
+private:
+    T m_x;
+    U m_y;
+
+public:
+    Pair(const T& x, const U& y)
+        : m_x{ x }, m_y{ y }
+    {
+    }
+
+    const T& first() const { return m_x; }
+    const U& second() const { return m_y; }
+}; 
+
+// [Listing M]
+int main()
+{
+    StringValuePair<int> svp{ "Hello", 5 };
+    std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
+
+    return 0;
+}
