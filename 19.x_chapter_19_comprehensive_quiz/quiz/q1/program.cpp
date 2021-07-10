b9a4713b6e7b651ff9d8154151e816ad6f41e53d
jatrindo <jatrindo4096@gmail.com>
2021-07-10 13:20:09 -0500

    Tweak first draft solution for Q1 of Quiz 19.x, after referencing solution code

--- 19.x_chapter_19_comprehensive_quiz/quiz/q1/program.cpp
+++ 19.x_chapter_19_comprehensive_quiz/quiz/q1/program.cpp
@@ -22,13 +22,13 @@ private:
     T m_second;
 
 public:
-    Pair1(T first, T second)
+    Pair1(const T& first, const T& second)
         : m_first{ first }, m_second{ second }
     {
     }
 
-    T first() const { return m_first; }
-    T second() const { return m_second; }
+    T& first() const { return m_first; }
+    T& second() const { return m_second; }
 };
 
 // [Listing M]
