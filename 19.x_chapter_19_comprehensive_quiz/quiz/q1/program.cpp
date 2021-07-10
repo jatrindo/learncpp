9b86867b8af55e509f54b1a32256bfa6ac9d7ede
jatrindo <jatrindo4096@gmail.com>
2021-07-10 13:08:55 -0500

    Include problem statement, initial code, and Makefile for Q1 of Quiz 19.x

---
+++ 19.x_chapter_19_comprehensive_quiz/quiz/q1/program.cpp
@@ -0,0 +1,25 @@
+/*
+ *
+ * Its sometimes useful to define data that travels in pairs.
+ *
+ * Write a templated class named Pair1 that allows the user to define one
+ * template type that is used for both values in the pair. The following
+ * function should work: [Listing M]
+ * 
+ * and print:
+ * 
+ *  Pair: 5 8
+ *  Pair: 2.3 4.5
+ *
+*/
+// [Listing M]
+int main()
+{
+    Pair1<int> p1(5, 8);
+    std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
+
+    const Pair1<double> p2(2.3, 4.5);
+    std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
+
+    return 0;
+}
