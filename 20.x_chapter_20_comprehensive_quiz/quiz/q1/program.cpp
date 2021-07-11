d538ca0b2a78892e9135f63ca592df8ee11e53f0
jatrindo <jatrindo4096@gmail.com>
2021-07-11 12:56:34 -0500

    Include first draft solution for Q1 of Quiz 20.x

--- 20.x_chapter_20_comprehensive_quiz/quiz/q1/program.cpp
+++ 20.x_chapter_20_comprehensive_quiz/quiz/q1/program.cpp
@@ -9,10 +9,61 @@
  * valid, print the fraction. If the Fraction is invalid, catch a
  * std::exception, and tell the user that they entered an invalid fraction.
  *
- * Hereś what one run of the program should output:
+ * Here's what one run of the program should output:
  * 
  *  Enter the numerator: 5
  *  Enter the deonminator: 0
  *  Invalid denominator
  *
 */
+#include <iostream>
+#include <stdexcept>
+
+class Fraction
+{
+private:
+    int m_top;
+    int m_bot;
+
+public:
+    Fraction(int top, int bot) try
+        : m_top{ top }, m_bot{ bot }
+    {
+        if (bot == 0)
+            throw std::runtime_error("Invalid denominator");
+    }
+    catch (std::runtime_error& e)
+    {
+        throw;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, Fraction& f)
+    {
+        std::cout << f.m_top << '/' << f.m_bot;
+        return out;
+    }
+};
+
+int main()
+{
+    // Ask two integers:
+    std::cout << "Enter the numerator: ";
+    int numerator;
+    std::cin >> numerator;
+
+    std::cout << "Enter the denominator: ";
+    int denominator;
+    std::cin >> denominator;
+
+    try
+    {
+        Fraction f{ numerator, denominator };
+        std::cout << f << '\n';
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return 0;
+}
