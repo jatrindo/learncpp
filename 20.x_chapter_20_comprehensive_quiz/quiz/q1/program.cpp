f1e5414535b777fafe4334f49ed98e54fba5b9f7
jatrindo <jatrindo4096@gmail.com>
2021-07-11 12:38:51 -0500

    Include problem statement, initial code, and Makefile for Q1 of Quiz 20.x

---
+++ 20.x_chapter_20_comprehensive_quiz/quiz/q1/program.cpp
@@ -0,0 +1,18 @@
+/*
+ * Write a Fraction class that has a constructor that takes a numerator and a
+ * denominator.
+ *
+ * If the user passes in a denominator of 0, throw an exception of type
+ * std::runtime_error (included in the stdexcept header).
+ *
+ * In your main program, ask the user to enter two integers. If the Fraction is
+ * valid, print the fraction. If the Fraction is invalid, catch a
+ * std::exception, and tell the user that they entered an invalid fraction.
+ *
+ * Hereś what one run of the program should output:
+ * 
+ *  Enter the numerator: 5
+ *  Enter the deonminator: 0
+ *  Invalid denominator
+ *
+*/
